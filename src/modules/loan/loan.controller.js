854bd7347834c55bc5f7dd2616d28cec7a89a0e8
myunus-info <myunus_info@yahoo.com>
2023-11-08 10:09:04 +0600

    create-loan function done

--- src/modules/loan/loan.controller.js
+++ src/modules/loan/loan.controller.js
@@ -2,6 +2,13 @@ const path = require('path');
 const Loan = require('./loan.model');
 const Customer = require(path.join(process.cwd(), 'src/modules/customer/customer.model'));
 const { asyncHandler, AppError } = require(path.join(process.cwd(), '/src/modules/core/errors'));
+const {
+  calculateCreditScore,
+  determineLoanEligibility,
+  calculateTotalCurrentLoanEMIs,
+  calculateMonthlyInstallment,
+  getLoanDataFromXLfile,
+} = require('./loan.helpers');
 
 exports.createLoan = asyncHandler(async (req, res, next) => {
   const { customer_id, loan_id, interest_rate, tenure } = req.body;
@@ -11,5 +18,39 @@ exports.createLoan = asyncHandler(async (req, res, next) => {
     return next(new AppError(404, 'No customer found!'));
   }
 
-  res.status(201).json({ msg: 'success' });
+  const loans = getLoanDataFromXLfile();
+  const customerLoans = loans.filter(loan => loan.customer_id === customer.customer_id);
+  if (!customerLoans || customerLoans.length < 1) {
+    return next(new AppError(500, 'No loans found for this customer!'));
+  }
+
+  const creditScore = calculateCreditScore(customer, customerLoans);
+  const totalCurrentLoanEMI = calculateTotalCurrentLoanEMIs(customerLoans);
+  const monthlyInstallment = calculateMonthlyInstallment(customerLoans);
+
+  const { approval } = determineLoanEligibility(
+    creditScore,
+    interest_rate,
+    customer,
+    totalCurrentLoanEMI
+  );
+
+  if (approval) {
+    const newLoan = await Loan.create({ customer_id, loan_id, interest_rate, tenure });
+    res.status(201).json({
+      loan_id: newLoan.loan_id,
+      customer_id: newLoan.customer_id,
+      loan_approved: approval,
+      message: 'Loan created successfully!',
+      monthly_installment: monthlyInstallment,
+    });
+  } else {
+    res.status(403).json({
+      loan_id,
+      customer_id,
+      loan_approved: approval,
+      message: `The loan for customer Id: ${customer_id} has not been approved!`,
+      monthly_installment: 0,
+    });
+  }
 });
--- src/modules/loan/loan.helpers.js
+++ src/modules/loan/loan.helpers.js
@@ -6,6 +6,101 @@ function getLoanDataFromXLfile() {
   return XLSX.utils.sheet_to_json(loanData.Sheets['Sheet1']);
 }
 
+function calculateCreditScore(customer, historicalLoans) {
+  let creditScore = 0;
+
+  // Past Loans paid on time
+  let totalEMIs = 0;
+  let totalEMIsPaidOnTime = 0;
+  for (const loan of historicalLoans) {
+    totalEMIs += loan.tenure;
+    totalEMIsPaidOnTime += loan['EMIs paid on Time'];
+  }
+  const percentageOfEMIsPaidOnTime = (totalEMIsPaidOnTime / totalEMIs) * 100;
+  if (percentageOfEMIsPaidOnTime === 100) creditScore += 40;
+  else if (percentageOfEMIsPaidOnTime >= 90) creditScore += 30;
+  else if (percentageOfEMIsPaidOnTime >= 80) creditScore += 20;
+
+  // No of loans taken in past
+  creditScore += Math.min(historicalLoans.length * 5, 30);
+
+  // Loan activity in current year
+  const loansInCurrentYear = historicalLoans.filter(
+    loan => new Date(loan.start_date).getFullYear() === new Date().getFullYear()
+  );
+  creditScore -= loansInCurrentYear.length * 5;
+
+  // Loan approved volume
+  let totalLoanAmount = 0;
+  for (const loan of historicalLoans) {
+    totalLoanAmount += loan.loan_amount;
+  }
+  const creditUtilizationRatio = totalLoanAmount / customer.approved_limit;
+  if (creditUtilizationRatio <= 0.3) creditScore += 30;
+  else if (creditUtilizationRatio <= 0.5) creditScore += 25;
+  else if (creditUtilizationRatio <= 0.7) creditScore += 20;
+
+  // If sum of current loans of customer > approved limit of customer, credit score = 0
+  if (loansInCurrentYear.length > customer.approved_limit) {
+    creditScore = 0;
+  }
+
+  // Credit Score should be a whole number not a fraction
+  return Math.floor(creditScore);
+}
+
+function calculateTotalCurrentLoanEMIs(historicalLoans) {
+  let totalCurrentLoanEMI = 0;
+  const currentYearStart = new Date(new Date().getFullYear(), 0, 1).getTime();
+  const currentYearEnd = new Date(new Date().getFullYear(), 11, 31).getTime();
+  for (const loan of historicalLoans) {
+    if (
+      new Date(loan.start_date).getTime() <= currentYearStart &&
+      new Date(loan.end_date).getTime() >= currentYearEnd
+    ) {
+      totalCurrentLoanEMI += loan.monthly_payment;
+    }
+  }
+  return totalCurrentLoanEMI;
+}
+
+function calculateMonthlyInstallment(loanAmount, interestRate, tenure) {
+  const monthlyInterestRate = interestRate / 12 / 100;
+  const emi =
+    (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, tenure)) /
+    (Math.pow(1 + monthlyInterestRate, tenure) - 1);
+
+  return +emi.toFixed(2);
+}
+
+function determineLoanEligibility(creditScore, interest_rate, customer, totalCurrentLoanEMI) {
+  let interestRate = interest_rate;
+  let approval = false;
+
+  if (creditScore > 50) {
+    interestRate = interest_rate;
+    approval = true;
+  } else if (creditScore < 50 && creditScore > 30) {
+    interestRate > 12 ? (interestRate = interest_rate) : (interestRate = 12);
+    approval = true;
+  } else if (creditScore < 30 && creditScore > 10) {
+    interestRate > 16 ? (interestRate = interest_rate) : (interestRate = 16);
+    approval = true;
+  } else if (totalCurrentLoanEMI > 0.5 * customer.monthly_salary) {
+    interestRate = 0;
+    approval = false;
+  } else {
+    interestRate = 0;
+    approval = false;
+  }
+
+  return { interestRate, approval };
+}
+
 module.exports = {
+  calculateCreditScore,
+  calculateTotalCurrentLoanEMIs,
+  calculateMonthlyInstallment,
   getLoanDataFromXLfile,
+  determineLoanEligibility,
 };
